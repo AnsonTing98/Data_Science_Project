e1efb1844687eb4ac7066bb1560977b8a91f19e2
MobDev1130 <l00138041@student.lyit.ie>
2022-03-24 12:22:40 +0000

    Week 3

---
+++ Merging_dataset_Practical.R
@@ -0,0 +1,37 @@
+# Load the dataset into data frame 
+# called new_managers_data
+new_managers_data <- read.csv("MoreData.csv")
+new_managers_data
+str(new_managers_data)
+
+# Select a subset of the data you need
+new_data <- subset(new_managers_data, select = c("Date", 
+                                                 "Country", 
+                                                 "Gender", 
+                                                 "Age", 
+                                                 "Q1", 
+                                                 "Q2", 
+                                                 "Q3", 
+                                                 "Q4", 
+                                                 "Q5"))
+new_data
+
+new_data$AgeCat[new_data$Age >= 45] <- "Elder"
+new_data$AgeCat[new_data$Age >= 26 & new_data$Age <= 44] <- "Middle Aged"
+new_data$AgeCat[new_data$Age <= 25] <- "Young"
+new_data$AgeCat[is.na(new_data$Age)] <- "Elder"
+
+factor_AgeCat <- factor(new_data$AgeCat, 
+                        ordered = TRUE, 
+                        levels = c("Young", "Middle Aged", "Elder"))
+new_data$AgeCat <- factor_AgeCat
+
+managers_data <- managers_data[-1]
+
+str(managers_data)
+merged_manager_data <- rbind(new_data,managers_data)
+merged_manager_data
+
+str(merged_manager_data)
+
+
---
+++ Week3.R
@@ -0,0 +1,50 @@
+# Load the managers dataset
+managers_data <- read.csv("managers.csv", na = "")
+managers_data
+str(managers_data)
+
+# Doesnt work with chr
+# so needs to be converted to int
+managers_data$Age <- as.integer((managers_data$Age))
+managers_data$Q4 <- as.integer(managers_data$Q4)
+managers_data$Q5 <- as.integer(managers_data$Q5)
+
+# Dealing with missing data
+
+# Listwise delection
+new_date <- na.omit(managers_data)
+new_date
+
+# Use complete.cases to show where data
+# rows are complete
+complete_data <- complete.cases(managers_data)
+complete_data
+sum(complete_data)
+
+# List the rows where date is not missing
+complete_data <- managers_data[complete.cases(managers_data),]
+complete_data
+
+# list incomplete cases
+incomplete_data <- managers_data[!complete.cases(managers_data),]
+incomplete_data
+
+# Find the sum of missing values
+sum(is.na(managers_data))
+
+mean(is.na(managers_data))
+
+install.packages("mice")
+library(mice)
+
+md.pattern(managers_data)
+
+install.packages("lmtest", dependencies = TRUE)
+library(lmtest)
+
+# Use VIM package to show missing values
+install.packages("VIM")
+library(VIM)
+
+missing_values <- aggr(managers_data, prop = FALSE, numbers = TRUE)
+
